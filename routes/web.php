44af0daf2ae637128f59423d7aea6d33ba90881b
M. Burhanudin Yusuf <burhanyusuf@outlook.com>
2025-12-03 22:17:41 +0700

    feat: Update Docker configuration for PHP 8.4 and add database creation scripts for MySQL, MariaDB, and PostgreSQL

--- routes/web.php
+++ routes/web.php
@@ -31,3 +31,7 @@ require __DIR__ . '/settings.php';
 Route::get('/', [FrontController::class, 'livestream'])->name('live');
 
 Route::post('live', [FrontController::class, 'joinLivestream'])->name('join-livestream');
+
+Route::get('/info', function () {
+    phpinfo();
+});
