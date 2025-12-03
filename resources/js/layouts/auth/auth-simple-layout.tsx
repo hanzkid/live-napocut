2c7b6fe94bc9bc29850ceaad5a13a79f7122ff16
M. Burhanudin Yusuf <burhanyusuf@outlook.com>
2025-12-03 20:29:48 +0700

    feat: Add new favicon and logo SVG files, update app layout to use new logo and favicon links

--- resources/js/layouts/auth/auth-simple-layout.tsx
+++ resources/js/layouts/auth/auth-simple-layout.tsx
@@ -18,13 +18,13 @@ export default function AuthSimpleLayout({
         <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-background p-6 md:p-10">
             <div className="w-full max-w-sm">
                 <div className="flex flex-col gap-8">
-                    <div className="flex flex-col items-center gap-4">
+                    <div className="flex flex-col items-center gap-8">
                         <Link
                             href={dashboard()}
-                            className="flex flex-col items-center gap-2 font-medium"
+                            className="flex flex-col items-center font-medium"
                         >
-                            <div className="mb-1 flex h-9 w-9 items-center justify-center rounded-md">
-                                <AppLogoIcon className="size-9 fill-current text-[var(--foreground)] dark:text-white" />
+                            <div className="flex h-32 w-32 items-center justify-center rounded-md">
+                                <AppLogoIcon className="size-32 fill-current text-[var(--foreground)] dark:text-white" />
                             </div>
                             <span className="sr-only">{title}</span>
                         </Link>
